976bbbb74bac82e668eb081a33bf01e4298df0a2
Evan Allan <evanallan1026@gmail.com>
2022-05-06 15:39:37 -0500

    Add dyslexia button

--- lab06/03-dyslexia-mode/index.js
+++ lab06/03-dyslexia-mode/index.js
@@ -1,4 +1,15 @@
-/* 
-  See Smashing Magazine Tutorial:
-  https://www.smashingmagazine.com/2021/11/dyslexia-friendly-mode-website/
-*/
+function toggleDyslexia() {
+    let isPressed = window.localStorage.getItem('dyslexic') === 'true';
+    setDyslexia(!isPressed);
+}
+
+function setDyslexia(isDyslexia) {
+    document.getElementById('dyslexia-toggle').setAttribute('aria-pressed', isDyslexia);
+    if (document.body.classList.contains('dyslexia-mode') !== isDyslexia) {
+        document.body.classList.toggle('dyslexia-mode');
+    }
+    window.localStorage.setItem('dyslexic', String(isDyslexia));
+}
+
+document.querySelector("#dyslexia-toggle").addEventListener('click', toggleDyslexia);
+setDyslexia(window.localStorage.getItem('dyslexic') === 'true');
