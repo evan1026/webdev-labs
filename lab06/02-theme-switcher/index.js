b7bcbd0b1e98cffe2bf8e0dbf0e17724230054af
Evan Allan <evanallan1026@gmail.com>
2022-05-06 15:20:14 -0500

    Implement themes

--- lab06/02-theme-switcher/index.js
+++ lab06/02-theme-switcher/index.js
@@ -1,7 +1,8 @@
-/*
-    Hints: 
-    1. Attach click event handlers to all four of the 
-       buttons (#default, #ocean, #desert, and #high-contrast).
-    2. Modify the className property of the body tag 
-       based on the button that was clicked.
-*/
+function setTheme(theme) {
+    document.querySelector("body").className = theme;
+}
+
+document.querySelector("#default").addEventListener('click', () => setTheme(""));
+document.querySelector("#desert").addEventListener('click', () => setTheme("desert"));
+document.querySelector("#ocean").addEventListener('click', () => setTheme("ocean"));
+document.querySelector("#high-contrast").addEventListener('click', () => setTheme("high-contrast"));
